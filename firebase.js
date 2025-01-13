eb1d5a60be1a714176aa3c7255fe99926662fd31
Korir <koriremanuel0@gmail.com>
2025-01-13 13:54:06 +0300

    Portfolio ready to deploy

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC37THWmcoYYI-YaNAA78q4zN9p7D8Ul4Y",
   authDomain: "portfolio-korir.firebaseapp.com",
